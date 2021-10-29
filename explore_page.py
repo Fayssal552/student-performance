6d58911a76318f46a3ecbd32ce6a756b87c0154e
Fayssal552 <75565585+Fayssal552@users.noreply.github.com>
2021-10-29 13:11:17 +0100

    Update explore_page.py

--- explore_page.py
+++ explore_page.py
@@ -8,12 +8,10 @@ import matplotlib.pyplot as plt
 def load_data():
     df_train = pd.read_csv('student-mat.csv')
 
-    df_train['schoolsup'] = df_train['schoolsup'].replace({'yes':1, 'no':0})
+    
     df_train['famsup'] = df_train['famsup'].replace({'yes':1, 'no':0})
-    df_train['paid'] = df_train['paid'].replace({'yes':1, 'no':0})
     df_train['activities'] = df_train['activities'].replace({'yes':1, 'no':0})
     df_train['higher'] = df_train['higher'].replace({'yes':1, 'no':0})
-    df_train['internet'] = df_train['internet'].replace({'yes':1, 'no':0})
     df_train['romantic'] = df_train['romantic'].replace({'yes':1, 'no':0})
 
     return df_train
@@ -21,19 +19,36 @@ def load_data():
 df_train = load_data()
 
 def show_explore_page():
-    st.title("Explore Software Engineer Salaries")
+    st.title("Explore Student Grade")
 
+    
     st.write(
         """
-    ### Stack Overflow Developer Survey 2020
+    #### Mean final grade Based On weekly study time 
     """
     )
     st.write(
         """
-    #### Mean Salary Based On Country
+    ###### weekly study time |1(<2h)| and |2(2h to 5h)|and | 3(5h to 10h)| and|4(10h to 20h)|
     """
     )
 
     
     data = df_train.groupby(["studytime"])["G3"].mean().sort_values(ascending=True)
-    st.line_chart(data)
+    st.bar_chart(data)
+
+    st.write(
+        """
+    #### Mean final grade Based On romantic relationship 
+    """
+    )
+    st.write(
+        """
+    ###### 0 : NONE  -1 : yes but not in same shool -2 : yes, in same chool
+    """
+    )
+    dat = df_train.groupby(["romantic"])["G3"].mean().sort_values(ascending=True)
+    st.bar_chart(dat)
+    
+
+    
